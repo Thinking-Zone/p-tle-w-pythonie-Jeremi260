3b217f5b0ee6b4c4e461553da1c4240ec8b363c2
Jeremi260 <jbbirula@gmail.com>
2024-11-07 09:48:49 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,12 @@
+# Pętla "for" jest zazwyczaj używana, gdy znamy liczbę iteracji lub gdy chcemy iterować po elementach kolekcji (np. listy, krotki, ciągi znaków, zakresy itp.)
+# Przykład pętli for:
+# for i in range(5):  # iteracja po liczbach 0 do 4
+#     print(i)
+
+# Pętla "while" jest używana, gdy chcemy, aby pętla wykonywała się dopóki nie spełni się jakiś warunek. 
+# Używamy jej, gdy nie znamy dokładnej liczby iteracji na początku i zależymy od jakiegoś warunku.
+# Przykład pętli while:
+# i = 0
+# while i < 5:  # pętla będzie się wykonywać, dopóki i jest mniejsze od 5
+#     print(i)
+#     i += 1
